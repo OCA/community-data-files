581685b98f12779626b08e4b236697a423a54b8b
Alexis de Lattre <alexis.delattre@akretion.com>
2016-03-21 00:56:14 +0100

    Add modules base_unece, account_tax_unece, account_payment_unece and product_uom_unece

---
+++ account_tax_unece/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# © 2016 Akretion (http://www.akretion.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# @author Alexis de Lattre <alexis.delattre@akretion.com>
+
+{
+    'name': 'Account Tax UNECE',
+    'version': '8.0.1.0.0',
+    'category': 'Accounting & Finance',
+    'license': 'AGPL-3',
+    'summary': 'UNECE nomenclature for taxes',
+    'author': 'Akretion,Odoo Community Association (OCA)',
+    'website': 'http://www.akretion.com',
+    'depends': ['account', 'base_unece'],
+    'data': [
+        'views/account_tax.xml',
+        'data/unece_tax_type.xml',
+        'data/unece_tax_categ.xml',
+        ],
+    'installable': True,
+}
