8f219911504490b4914e2fe52d6767754fd23fac
Alexis de Lattre <alexis.delattre@akretion.com>
2016-11-19 01:51:03 +0100

    [MIG] base_unece: Migration to 10.0

--- base_unece/__manifest__.py
+++ base_unece/__manifest__.py
@@ -5,7 +5,7 @@
 
 {
     'name': 'Base UNECE',
-    'version': '8.0.1.0.0',
+    'version': '10.0.1.0.0',
     'category': 'Tools',
     'license': 'AGPL-3',
     'summary': 'Base module for UNECE code lists',
