b24aa2fedc22a22e092f28a50d6945fe9dd0cc77
etobella <etobella@creublanca.es>
2017-06-19 16:19:19 +0200

    [MIG] base_3166: Migration to 10.0

--- base_iso3166/__manifest__.py
+++ base_iso3166/__manifest__.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 # © 2016 Serv. Tecnol. Avanzados - Pedro M. Baeza
+# © 2017  Creu Blanca <www.creublanca.es>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "ISO 3166",
-    "version": "8.0.1.0.0",
-    "author": "Serv. Tecnol. Avanzados - Pedro M. Baeza, "
+    "version": "10.0.1.0.0",
+    "author": "Tecnativa, "
+              "Creu Blanca, "
               "Odoo Community Association (OCA)",
     "category": "Localization",
     "website": "https://odoo-community.org",
