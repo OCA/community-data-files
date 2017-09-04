44b5ccab7cea78b49d339f6066f351359a2c62fc
Andrea <a.stirpe@onestein.nl>
2017-09-04 16:30:00 +0200

    Add unece type and category to tax template

---
+++ account_tax_unece/models/account_tax_template.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Onestein (<http://www.onestein.eu>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import fields, models
+
+
+class AccountTaxTemplate(models.Model):
+    _inherit = 'account.tax.template'
+
+    unece_type_id = fields.Many2one(
+        'unece.code.list', string='UNECE Tax Type',
+        domain=[('type', '=', 'tax_type')],
+        help="Select the Tax Type Code of the official "
+        "nomenclature of the United Nations Economic "
+        "Commission for Europe (UNECE), DataElement 5153"
+    )
+    unece_categ_id = fields.Many2one(
+        'unece.code.list', string='UNECE Tax Category',
+        domain=[('type', '=', 'tax_categ')],
+        help="Select the Tax Category Code of the official "
+        "nomenclature of the United Nations Economic "
+        "Commission for Europe (UNECE), DataElement 5305"
+    )
+
+    def _get_tax_vals(self, company):
+        self.ensure_one()
+        res = super(AccountTaxTemplate, self)._get_tax_vals(company)
+        res['unece_type_id'] = self.unece_type_id.id or False
+        res['unece_categ_id'] = self.unece_categ_id.id or False
+        return res
