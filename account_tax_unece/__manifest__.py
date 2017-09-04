8a50f5fa37ff847989b306baebd59a87aa585d2b
Andrea <a.stirpe@onestein.nl>
2017-09-04 16:30:00 +0200

    Add unece type and category to tax template

--- account_tax_unece/__manifest__.py
+++ account_tax_unece/__manifest__.py
@@ -5,7 +5,7 @@
 
 {
     'name': 'Account Tax UNECE',
-    'version': '10.0.1.0.0',
+    'version': '10.0.1.1.0',
     'category': 'Accounting & Finance',
     'license': 'AGPL-3',
     'summary': 'UNECE nomenclature for taxes',
@@ -14,6 +14,7 @@
     'depends': ['account', 'base_unece'],
     'data': [
         'views/account_tax.xml',
+        'views/account_tax_template.xml',
         'data/unece_tax_type.xml',
         'data/unece_tax_categ.xml',
         ],
