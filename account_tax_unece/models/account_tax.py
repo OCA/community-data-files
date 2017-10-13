db0b232d5239d055d95515a85e03ebd0b8f0094a
Alexis de Lattre <alexis@via.ecp.fr>
2017-10-13 21:41:23 +0200

    [10.0] Add field 'UNECE Due date' on taxes and corresponding nomenclature (#26)

--- account_tax_unece/models/account_tax.py
+++ account_tax_unece/models/account_tax.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# © 2016 Akretion (http://www.akretion.com)
+# © 2016-2017 Akretion (http://www.akretion.com)
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
@@ -16,7 +16,7 @@ class AccountTax(models.Model):
         "nomenclature of the United Nations Economic "
         "Commission for Europe (UNECE), DataElement 5153")
     unece_type_code = fields.Char(
-        related='unece_type_id.code', store=True,
+        related='unece_type_id.code', store=True, readonly=True,
         string='UNECE Type Code')
     unece_categ_id = fields.Many2one(
         'unece.code.list', string='UNECE Tax Category',
@@ -25,5 +25,17 @@ class AccountTax(models.Model):
         "nomenclature of the United Nations Economic "
         "Commission for Europe (UNECE), DataElement 5305")
     unece_categ_code = fields.Char(
-        related='unece_categ_id.code', store=True,
+        related='unece_categ_id.code', store=True, readonly=True,
         string='UNECE Category Code')
+    unece_due_date_id = fields.Many2one(
+        'unece.code.list', string='UNECE Due Date',
+        domain=[('type', '=', 'date')], ondelete='restrict',
+        help="Select the due date of that tax from the official "
+        "nomenclature of the United Nations Economic "
+        "Commission for Europe (UNECE), DataElement 2005. For a "
+        "sale VAT tax, it is the date on which that VAT is due to the "
+        "fiscal administration. For a purchase VAT tax, it is the date "
+        "on which that VAT can be deducted.")
+    unece_due_date_code = fields.Char(
+        related='unece_due_date_id.code', store=True, readonly=True,
+        string='UNECE Due Date Code')
