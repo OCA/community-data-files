12d377ac6e48dde55234f043b09bc06953cbe90f
Alexis de Lattre <alexis@via.ecp.fr>
2017-10-13 21:41:23 +0200

    [10.0] Add field 'UNECE Due date' on taxes and corresponding nomenclature (#26)

--- account_tax_unece/models/unece_code_list.py
+++ account_tax_unece/models/unece_code_list.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# © 2016 Akretion (http://www.akretion.com)
+# © 2016-2017 Akretion (http://www.akretion.com)
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
@@ -12,4 +12,5 @@ class UneceCodeList(models.Model):
     type = fields.Selection(selection_add=[
         ('tax_type', 'Tax Types (UNCL 5153)'),
         ('tax_categ', 'Tax Categories (UNCL 5305)'),
+        ('date', 'Date, Time or Period Qualifier (UNTDID 2005)'),
         ])
