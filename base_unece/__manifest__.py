7df9e0a1fb8bc2f94aca403e564354578997865c
Andrea <a.stirpe@onestein.nl>
2018-01-17 14:44:05 +0100

    [11.0][MIG] base_unece: porting + minor changes

--- base_unece/__manifest__.py
+++ base_unece/__manifest__.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
-# © 2016 Akretion (http://www.akretion.com)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# Copyright 2016 Akretion (http://www.akretion.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 # @author Alexis de Lattre <alexis.delattre@akretion.com>
 
 {
     'name': 'Base UNECE',
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'category': 'Tools',
     'license': 'AGPL-3',
     'summary': 'Base module for UNECE code lists',
