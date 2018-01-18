d23cd3567182730250507c2f156f8f3a8b051db9
Andrea <a.stirpe@onestein.nl>
2018-01-18 16:03:55 +0100

    [11.0][MIG] account_tax_unece: porting to V11

--- account_tax_unece/models/account_tax.py
+++ account_tax_unece/models/account_tax.py
@@ -1,7 +1,6 @@
-# -*- coding: utf-8 -*-
-# © 2016-2017 Akretion (http://www.akretion.com)
+# Copyright 2016-2017 Akretion (http://www.akretion.com)
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo import models, fields
 
