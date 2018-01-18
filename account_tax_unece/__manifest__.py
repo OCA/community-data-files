843fc2b526ee65b99b16682c37f14e8a8f7180c6
Andrea <a.stirpe@onestein.nl>
2018-01-18 16:03:55 +0100

    [11.0][MIG] account_tax_unece: porting to V11

--- account_tax_unece/__manifest__.py
+++ account_tax_unece/__manifest__.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
-# © 2016 Akretion (http://www.akretion.com)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# Copyright 2016 Akretion (http://www.akretion.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 # @author Alexis de Lattre <alexis.delattre@akretion.com>
 
 {
     'name': 'Account Tax UNECE',
-    'version': '10.0.1.1.1',
+    'version': '11.0.1.1.1',
     'category': 'Accounting & Finance',
     'license': 'AGPL-3',
     'summary': 'UNECE nomenclature for taxes',
