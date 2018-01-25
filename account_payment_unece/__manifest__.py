c90891178c6f5b2123bbf38ec88b55eba79c74e2
Andrea <a.stirpe@onestein.nl>
2018-01-25 16:18:30 +0100

    [11.0][MIG] account_payment_unece: port to V11

--- account_payment_unece/__manifest__.py
+++ account_payment_unece/__manifest__.py
@@ -1,17 +1,20 @@
-# -*- coding: utf-8 -*-
-# © 2016 Akretion (http://www.akretion.com)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# Copyright 2016 Akretion (http://www.akretion.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 # @author Alexis de Lattre <alexis.delattre@akretion.com>
 
 {
     'name': 'Account Payment UNECE',
-    'version': '10.0.1.0.1',
+    'version': '11.0.1.0.0',
     'category': 'Accounting & Finance',
     'license': 'AGPL-3',
     'summary': 'UNECE nomenclature for the payment methods',
     'author': 'Akretion,Odoo Community Association (OCA)',
     'website': 'http://www.akretion.com',
-    'depends': ['account_payment_mode', 'base_unece'],
+    'depends': [
+        'account_payment_mode',
+        'base_unece',
+        'account_tax_unece'
+    ],
     'data': [
         'data/unece.xml',
         'data/account_payment_method.xml',
--- account_payment_unece/tests/test_account_invoice.py
+++ account_payment_unece/tests/test_account_invoice.py
@@ -1,6 +1,5 @@
-# -*- coding: utf-8 -*-
-# © 2017 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# Copyright 2017 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo.tests.common import HttpCase
 
