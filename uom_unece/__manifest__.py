3cde18455b0634995dbfc0f56d0ac98fb5926580
Alexis de Lattre <alexis.delattre@akretion.com>
2018-06-15 23:11:51 +0200

    [IMP] product_uom_unece: Add UNECE codes for american UoM

--- uom_unece/__manifest__.py
+++ uom_unece/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     'name': 'Product UoM UNECE',
-    'version': '11.0.1.0.0',
+    'version': '11.0.1.1.0',
     'category': 'Sales',
     'license': 'AGPL-3',
     'summary': 'UNECE nomenclature for the units of measure',
