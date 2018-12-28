43a5f5d1d9adae8ed7f6d63dad8c82334416b8b0
sbejaoui <souheil.bejaoui@acsone.eu>
2018-12-28 20:16:50 +0100

    [MIG] - migration l10n_eu_nace to 12.0
    [ADD] setup.py

--- l10n_eu_nace/data/make_data.py
+++ l10n_eu_nace/data/make_data.py
@@ -1,41 +1,46 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2011 Numérigraphe SARL.
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright 2011 Numérigraphe SARL.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
 
 import csv
 
 # List of languages to generate translations for
 LANGS = [
-    'bg', 'cs', 'da', 'de', 'et', 'en', 'es', 'el', 'fi', 'fr', 'hr', 'hu',
-    'it', 'lt', 'lv', 'mt', 'nl', 'no', 'pl', 'pt', 'ro', 'ru', 'sk', 'sl',
-    'sv', 'tr',
+    'bg',
+    'cs',
+    'da',
+    'de',
+    'et',
+    'en',
+    'es',
+    'el',
+    'fi',
+    'fr',
+    'hr',
+    'hu',
+    'it',
+    'lt',
+    'lv',
+    'mt',
+    'nl',
+    'no',
+    'pl',
+    'pt',
+    'ro',
+    'ru',
+    'sk',
+    'sl',
+    'sv',
+    'tr',
 ]
 
 # All the generated record ids will be in this forms
 ID_TEMPLATE = "nace_%s"
 
-print "Generating the English CSV file..."
+print("Generating the English CSV file...")
 src = csv.reader(open("NACE_REV2_en.csv", "rU"))
-dest = csv.writer(open("res.partner.category.csv", "w"),
-                  quoting=csv.QUOTE_ALL)
+dest = csv.writer(open("res.partner.category.csv", "w"), quoting=csv.QUOTE_ALL)
 # Write the file header
 dest.writerow(["id", "parent_id:id", "name"])
 # Write the root category
@@ -55,17 +60,18 @@ for row in src:
     # Remember the English name and the id
     english[xml_id] = name
     dest.writerow([xml_id, parent_id, name])
-print "Done.\n"
+print("Done.\n")
 
 for lang in LANGS:
     filename = lang != "en" and ("%s.po" % lang) or "l10n_eu_nace.pot"
-    print "Generating %s..." % filename
+    print("Generating %s..." % filename)
     src = csv.reader(open("NACE_REV2_%s.csv" % lang, "rU"))
     # Skip first line
     src.next()
     # Write file header
     dest = open("../i18n/%s" % filename, "w")
-    dest.write("""# Translation of OpenERP Server.
+    dest.write(
+        """# Translation of OpenERP Server.
 # This file contains the translation of the following modules:
 #    * l10n_eu_nace
 #
@@ -82,14 +88,18 @@ msgstr ""
 "Content-Transfer-Encoding: \\n"
 "Plural-Forms: \\n"
 
-""")
+"""
+    )
     for row in src:
         name = "[%s] %s" % (row[1], row[2])
         xml_id = ID_TEMPLATE % row[1].replace('.', '_')
-        dest.write("""#. module: l10n_eu_nace
+        dest.write(
+            """#. module: l10n_eu_nace
 #: model:res.partner.category,name:l10n_eu_nace.%s
 msgid "%s"
 msgstr "%s"
 
-""" % (xml_id, english[xml_id], lang != "en" and name or ""))
-print "Done.\n"
+"""
+            % (xml_id, english[xml_id], lang != "en" and name or "")
+        )
+print("Done.\n")
