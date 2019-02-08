77e99dcf6b0e608bb48441e9ef7c44efbb767f38
remihb <remi@hobo.live>
2019-02-08 15:01:06 +0100

    [MIG] l10n_eu_nace: migration to 11.0

--- l10n_eu_nace/__manifest__.py
+++ l10n_eu_nace/__manifest__.py
@@ -21,7 +21,7 @@
 
 {
     "name": "European NACE partner categories",
-    "version": "10.0.1.0.0",
+    "version": "11.0.1.0.0",
     'author': u'Numérigraphe SARL,Sistheo,Odoo Community Association (OCA)',
     "category": "Localization",
     "data": [
