2bdd518ec609b4e69bd24c93a92b7a26349e00a4
Alexis de Lattre <alexis.delattre@akretion.com>
2019-02-20 11:50:31 +0100

    Add unece_due_date_id on account.tax.template

--- account_tax_unece/models/account_tax_template.py
+++ account_tax_unece/models/account_tax_template.py
@@ -21,6 +21,16 @@ class AccountTaxTemplate(models.Model):
         "nomenclature of the United Nations Economic "
         "Commission for Europe (UNECE), DataElement 5305"
     )
+    unece_due_date_id = fields.Many2one(
+        'unece.code.list', string='UNECE Due Date',
+        domain=[('type', '=', 'date')],
+        help="Select the due date of that tax from the official "
+        "nomenclature of the United Nations Economic "
+        "Commission for Europe (UNECE), DataElement 2005. For a "
+        "sale VAT tax, it is the date on which that VAT is due to the "
+        "fiscal administration. For a purchase VAT tax, it is the date "
+        "on which that VAT can be deducted."
+    )
 
     def _get_tax_vals(self, company, tax_template_to_tax):
         self.ensure_one()
@@ -30,4 +40,5 @@ class AccountTaxTemplate(models.Model):
         )
         res['unece_type_id'] = self.unece_type_id.id or False
         res['unece_categ_id'] = self.unece_categ_id.id or False
+        res['unece_due_date_id'] = self.unece_due_date_id.id or False
         return res
