972091f68b13af900c67a304ec9be69bd8921cc9
mreficent <miquel.raich@eficent.com>
2019-05-24 13:28:28 +0200

    [MIG] Add migration scripts

---
+++ account_payment_unece/migrations/12.0.1.0.0/post-migration.py
@@ -0,0 +1,9 @@
+# Copyright 2019 Eficent <http://www.eficent.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from openupgradelib import openupgrade
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    openupgrade.load_data(
+        env.cr, 'account_payment_unece', 'data/account_payment_method.xml')
