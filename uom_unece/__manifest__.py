b441eec8bab3b727e84f7b3ec13ce488fa1b69aa
Andrea <a.stirpe@onestein.nl>
2019-10-14 09:39:27 +0200

    [13.0][MIG] uom_unece

--- uom_unece/__manifest__.py
+++ uom_unece/__manifest__.py
@@ -3,17 +3,14 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Product UoM UNECE',
-    'version': '12.0.1.1.0',
-    'category': 'Sales',
-    'license': 'AGPL-3',
-    'summary': 'UNECE nomenclature for the units of measure',
-    'author': 'Akretion,Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/community-data-files/',
-    'depends': ['uom'],
-    'data': [
-        'data/unece.xml',
-        'views/product_uom.xml',
-        ],
-    'installable': True,
+    "name": "Product UoM UNECE",
+    "version": "13.0.1.0.0",
+    "category": "Sales",
+    "license": "AGPL-3",
+    "summary": "UNECE nomenclature for the units of measure",
+    "author": "Akretion,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/community-data-files/",
+    "depends": ["uom"],
+    "data": ["data/unece.xml", "views/product_uom.xml"],
+    "installable": True,
 }
