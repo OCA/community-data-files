a7c6bb9a18f8fcc6e92ac09813b6ddea68771ffc
Andrea <a.stirpe@onestein.nl>
2019-10-14 09:39:27 +0200

    [13.0][MIG] uom_unece

--- uom_unece/models/uom_uom.py
+++ uom_unece/models/uom_uom.py
@@ -2,13 +2,14 @@
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields
+from odoo import fields, models
 
 
 class UomUom(models.Model):
-    _inherit = 'uom.uom'
+    _inherit = "uom.uom"
 
     unece_code = fields.Char(
-        string='UNECE Code',
+        string="UNECE Code",
         help="Standard nomenclature of the United Nations Economic "
-        "Commission for Europe (UNECE).")
+        "Commission for Europe (UNECE).",
+    )
