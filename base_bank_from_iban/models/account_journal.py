56922d449e4c0c7cbb4f6a9e6a664623f6d0221c
emagdalena <emagdalena@c2i.es>
2019-10-18 22:20:58 +0200

    [IMP] base_bank_from_iban: black, isort

--- base_bank_from_iban/models/account_journal.py
+++ base_bank_from_iban/models/account_journal.py
@@ -5,17 +5,19 @@ from odoo import api, models
 
 
 class AccountJournal(models.Model):
-    _inherit = 'account.journal'
+    _inherit = "account.journal"
 
-    @api.onchange('bank_acc_number')
+    @api.onchange("bank_acc_number")
     def _onchange_bank_acc_number_base_bank_from_iban(self):
         if not self.bank_acc_number:
             return
-        partner_bank = self.env['res.partner.bank'].new({
-            'acc_number': self.bank_acc_number
-        })
+        partner_bank = self.env["res.partner.bank"].new(
+            {"acc_number": self.bank_acc_number}
+        )
         partner_bank._onchange_acc_number_base_bank_from_iban()
-        self.update({
-            'bank_acc_number': partner_bank.acc_number,
-            'bank_id': partner_bank.bank_id.id
-        })
+        self.update(
+            {
+                "bank_acc_number": partner_bank.acc_number,
+                "bank_id": partner_bank.bank_id.id,
+            }
+        )
