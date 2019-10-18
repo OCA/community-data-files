cf781015f651ef7c64328b075412f3afcf8fcd00
emagdalena <emagdalena@c2i.es>
2019-10-18 22:20:58 +0200

    [IMP] base_bank_from_iban: black, isort

--- base_bank_from_iban/__manifest__.py
+++ base_bank_from_iban/__manifest__.py
@@ -5,16 +5,11 @@
 {
     "name": "Bank from IBAN",
     "version": "12.0.1.0.0",
-    "author": "Tecnativa, "
-              "Odoo Community Association (OCA)",
+    "author": "Tecnativa, " "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/community-data-files",
     "category": "Localization",
     "license": "AGPL-3",
-    "depends": [
-        "base_iban",
-    ],
-    "data": [
-        "views/res_bank_view.xml",
-    ],
-    'installable': True,
+    "depends": ["base_iban"],
+    "data": ["views/res_bank_view.xml"],
+    "installable": True,
 }
