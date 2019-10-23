25b27d4df1552223ea8da2158e158e1e847f9a15
Andrea <a.stirpe@onestein.nl>
2019-10-23 11:11:42 +0200

    [13.0][MIG] account_tax_unece

--- account_tax_unece/__manifest__.py
+++ account_tax_unece/__manifest__.py
@@ -3,20 +3,20 @@
 # @author Alexis de Lattre <alexis.delattre@akretion.com>
 
 {
-    'name': 'Account Tax UNECE',
-    'version': '12.0.1.0.0',
-    'category': 'Accounting & Finance',
-    'license': 'AGPL-3',
-    'summary': 'UNECE nomenclature for taxes',
-    'author': 'Akretion,Odoo Community Association (OCA)',
-    'website': 'http://www.akretion.com',
-    'depends': ['account', 'base_unece'],
-    'data': [
-        'views/account_tax.xml',
-        'views/account_tax_template.xml',
-        'data/unece_tax_type.xml',
-        'data/unece_tax_categ.xml',
-        'data/unece_date.xml',
-        ],
-    'installable': True,
+    "name": "Account Tax UNECE",
+    "version": "13.0.1.0.0",
+    "category": "Accounting & Finance",
+    "license": "AGPL-3",
+    "summary": "UNECE nomenclature for taxes",
+    "author": "Akretion,Odoo Community Association (OCA)",
+    "website": "http://www.akretion.com",
+    "depends": ["account", "base_unece"],
+    "data": [
+        "views/account_tax.xml",
+        "views/account_tax_template.xml",
+        "data/unece_tax_type.xml",
+        "data/unece_tax_categ.xml",
+        "data/unece_date.xml",
+    ],
+    "installable": True,
 }
