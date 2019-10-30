cf2999cfab44034a8ad9dcb7434273d748d0b241
Andrea <a.stirpe@onestein.nl>
2019-10-30 11:27:48 +0100

    [13.0][MIG] account_payment_unece

--- account_payment_unece/__manifest__.py
+++ account_payment_unece/__manifest__.py
@@ -3,21 +3,18 @@
 # @author Alexis de Lattre <alexis.delattre@akretion.com>
 
 {
-    'name': 'Account Payment UNECE',
-    'version': '12.0.1.0.0',
-    'category': 'Accounting & Finance',
-    'license': 'AGPL-3',
-    'summary': 'UNECE nomenclature for the payment methods',
-    'author': 'Akretion,Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/community-data-files',
-    'depends': [
-        'account_payment_mode',
-        'base_unece',
+    "name": "Account Payment UNECE",
+    "version": "13.0.1.0.0",
+    "category": "Accounting & Finance",
+    "license": "AGPL-3",
+    "summary": "UNECE nomenclature for the payment methods",
+    "author": "Akretion,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/community-data-files",
+    "depends": ["account_payment_mode", "base_unece"],
+    "data": [
+        "data/unece.xml",
+        "data/account_payment_method.xml",
+        "views/account_payment_method.xml",
     ],
-    'data': [
-        'data/unece.xml',
-        'data/account_payment_method.xml',
-        'views/account_payment_method.xml',
-        ],
-    'installable': True,
+    "installable": True,
 }
