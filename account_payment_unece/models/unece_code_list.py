51959a5666b87dc425018c65e24b6bc11162edb2
Andrea <a.stirpe@onestein.nl>
2019-10-30 11:27:48 +0100

    [13.0][MIG] account_payment_unece

--- account_payment_unece/models/unece_code_list.py
+++ account_payment_unece/models/unece_code_list.py
@@ -2,11 +2,12 @@
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields
+from odoo import fields, models
 
 
 class UneceCodeList(models.Model):
-    _inherit = 'unece.code.list'
+    _inherit = "unece.code.list"
 
-    type = fields.Selection(selection_add=[
-        ('payment_means', 'Payment Means (UNCL 4461)')])
+    type = fields.Selection(
+        selection_add=[("payment_means", "Payment Means (UNCL 4461)")]
+    )
