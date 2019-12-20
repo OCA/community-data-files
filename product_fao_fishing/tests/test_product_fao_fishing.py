b4432b816dbbb4144bead7a2eab76c3e6404067e
Sergio Teruel <sergio.teruel@tecnativa.com>
2019-12-20 12:47:32 +0100

    [FIX] product_fao_fishing: product.attribute.line not exists

--- product_fao_fishing/tests/test_product_fao_fishing.py
+++ product_fao_fishing/tests/test_product_fao_fishing.py
@@ -8,8 +8,23 @@ class TestProductFaoFishing(SavepointCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
+        cls.product_attribute = cls.env.ref(
+            'product_fao_fishing.fao_fishing_area')
+        cls.product_attribute_value_1 = cls.env.ref(
+            'product_fao_fishing.fao_fishing_area_1')
+        cls.product_attribute_value_2 = cls.env.ref(
+            'product_fao_fishing.fao_fishing_area_2')
         cls.product = cls.env['product.template'].create({
             'name': 'test',
+            'attribute_line_ids': [
+                (0, 0, {
+                    'attribute_id': cls.product_attribute.id,
+                    'value_ids': [
+                        (4, cls.product_attribute_value_1.id),
+                        (4, cls.product_attribute_value_2.id),
+                    ],
+                }),
+            ],
         })
         cls.fishing_tech_arrow = cls.env.ref(
             'product_fao_fishing.fishing_technique_harrows')
@@ -26,3 +41,9 @@ class TestProductFaoFishing(SavepointCase):
     def test_recursion(self):
         with self.assertRaises(ValidationError):
             self.my_tech.parent_id = self.my_tech
+
+    def test_fishing_areas_from_attribute(self):
+        """
+        Test helper method to access directly to FAO fishing area product
+        """
+        self.assertEqual(len(self.product.fao_fishing_area_ids), 2)
