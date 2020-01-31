b9ebc2006c17d8e1e3bb82e05070657a7dcad37d
Thomas Binsfeld <thomas.binsfeld@acsone.eu>
2020-01-31 10:57:30 +0100

    [MIG] Migration of base_iso3166 from 12.0 to 13.0

--- base_iso3166/models/res_country.py
+++ base_iso3166/models/res_country.py
@@ -11,37 +11,39 @@ except ImportError:
 
 
 class ResCountry(models.Model):
-    _inherit = 'res.country'
+    _inherit = "res.country"
 
     code_alpha3 = fields.Char(
-        string='Country Code (3-letter)', size=3, store=True,
-        help='ISO 3166-1 alpha-3 (three-letter) code for the country',
-        compute="_compute_codes")
+        string="Country Code (3-letter)",
+        size=3,
+        store=True,
+        help="ISO 3166-1 alpha-3 (three-letter) code for the country",
+        compute="_compute_codes",
+    )
     code_numeric = fields.Char(
-        string='Country Code (numeric)', size=3, store=True,
-        help='ISO 3166-1 numeric code for the country',
-        compute="_compute_codes")
+        string="Country Code (numeric)",
+        size=3,
+        store=True,
+        help="ISO 3166-1 numeric code for the country",
+        compute="_compute_codes",
+    )
 
-    @api.multi
-    @api.depends('code')
+    @api.depends("code")
     def _compute_codes(self):
         for country in self:
             c = False
-            for country_type in ['countries', 'historic_countries']:
+            for country_type in ["countries", "historic_countries"]:
                 try:
-                    c = getattr(pycountry, country_type).get(
-                        alpha_2=country.code)
+                    c = getattr(pycountry, country_type).get(alpha_2=country.code)
                 except KeyError:
                     try:
-                        c = getattr(pycountry, country_type).get(
-                            alpha2=country.code)
+                        c = getattr(pycountry, country_type).get(alpha2=country.code)
                     except KeyError:
                         pass
                 if c:
                     break
             if c:
-                country.code_alpha3 = getattr(c, 'alpha_3',
-                                              getattr(c, 'alpha3', False))
+                country.code_alpha3 = getattr(c, "alpha_3", getattr(c, "alpha3", False))
                 country.code_numeric = c.numeric
             else:
                 country.code_alpha3 = False
--- base_iso3166/tests/test_iso3166.py
+++ base_iso3166/tests/test_iso3166.py
@@ -6,14 +6,13 @@ from odoo.tests import common
 
 class TestBaseIso3166(common.TransactionCase):
     def test_iso_3166(self):
-        country = self.env.ref('base.ad')
-        self.assertEqual(country.code_alpha3, 'AND')
-        self.assertEqual(country.code_numeric, '020')
+        country = self.env.ref("base.ad")
+        self.assertEqual(country.code_alpha3, "AND")
+        self.assertEqual(country.code_numeric, "020")
 
     def test_historic_countries(self):
-        ussr = self.env['res.country'].create({
-            'code': 'SU',
-            'name': 'USSR, Union of Soviet Socialist Republics',
-        })
-        self.assertEqual(ussr.code_alpha3, 'SUN')
-        self.assertEqual(ussr.code_numeric, '810')
+        ussr = self.env["res.country"].create(
+            {"code": "SU", "name": "USSR, Union of Soviet Socialist Republics"}
+        )
+        self.assertEqual(ussr.code_alpha3, "SUN")
+        self.assertEqual(ussr.code_numeric, "810")
