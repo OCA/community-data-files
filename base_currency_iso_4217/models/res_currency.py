472acad98184e4eac8f72685677a8e0a25384218
mreficent <miquel.raich@eficent.com>
2020-02-03 14:11:04 +0100

    [IMP] base_currency_iso_4217: black, isort, prettier

--- base_currency_iso_4217/models/res_currency.py
+++ base_currency_iso_4217/models/res_currency.py
@@ -2,19 +2,19 @@
 # @author: Jordi Ballester <jordi.ballester@eficent.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields
+from odoo import fields, models
 
 
 class ResCurrency(models.Model):
-    _inherit = 'res.currency'
+    _inherit = "res.currency"
 
     numeric_code = fields.Char(
-        string='Numeric Code',
+        string="Numeric Code",
         help="ISO Numeric Code for currency, according to ISO 4217 standard.",
     )
 
     full_name = fields.Char(
-        string='Full name',
+        string="Full name",
         help="Currency name, according to ISO 4217 standard",
         translate=True,
     )
