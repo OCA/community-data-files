67eed7883b9938645a096d23443c3a47a9cd888e
Iryna Vushnevska <i.vyshnevska@mobilunity.com>
2020-07-24 13:42:09 +0300

    [13.0][ADD] l10n_eu_product_adr: create

---
+++ l10n_eu_product_adr/__manifest__.py
@@ -0,0 +1,23 @@
+# Copyright 2019 Camptocamp SA
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
+{
+    "name": "ADR Products",
+    "summary": "Allows to set appropriate danger class and components",
+    "version": "13.0.1.0.0",
+    "category": "Product",
+    "website": "https://github.com/OCA/community-data-files",
+    "author": "Camptocamp, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": ["product", "sale", "stock"],
+    "data": [
+        "data/product_dangerous_type_data.xml",
+        "data/product_dangerous_class_data.xml",
+        "data/utility_models_data.xml",
+        "security/ir.model.access.csv",
+        "views/product_template_view.xml",
+        "views/product_dangerous.xml",
+        "views/utility_models.xml",
+    ],
+}
---
+++ l10n_eu_product_adr/models/product_dangerous.py
@@ -0,0 +1,27 @@
+# Copyright 2019 Iryna Vyshnevska (Camptocamp)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
+from odoo import fields, models
+
+
+class ProductDangerousClass(models.Model):
+    _name = "product.dangerous.class"
+    _description = "Product Dangerous Class"
+
+    name = fields.Char(required=True, translate=True)
+    code = fields.Char(required=True)
+    image = fields.Binary(string="Icon", required=True)
+    class_type_id = fields.Many2one(
+        comodel_name="product.dangerous.class.type",
+        ondelete="restrict",
+        string="Dangerous Type",
+    )
+
+    _sql_constraints = [("code_unique", "unique(code)", "This code already exist")]
+
+
+class ProductDangerousClassType(models.Model):
+    _name = "product.dangerous.class.type"
+    _description = "Product Dangerous Type"
+
+    name = fields.Char(required=True, translate=True)
+    division = fields.Char()
