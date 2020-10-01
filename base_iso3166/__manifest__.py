ce023ff0d10e1652a8bdb5448eed6282a4a7cd10
Ernesto Tejeda <ernesto.tejeda@tecnativa.com>
2020-10-01 11:58:38 -0400

    [IMP] base_iso3166: black, isort, prettier

--- base_iso3166/__manifest__.py
+++ base_iso3166/__manifest__.py
@@ -5,22 +5,12 @@
 {
     "name": "ISO 3166",
     "version": "12.0.1.0.0",
-    "author": "Tecnativa, "
-              "Creu Blanca, "
-              "Odoo Community Association (OCA)",
+    "author": "Tecnativa, " "Creu Blanca, " "Odoo Community Association (OCA)",
     "category": "Localization",
     "website": "https://github.com/OCA/community-data-files",
     "license": "AGPL-3",
-    "depends": [
-        "base"
-    ],
-    'external_dependencies': {
-        'python': [
-            'pycountry',
-        ],
-    },
-    "data": [
-        "views/country_view.xml",
-    ],
+    "depends": ["base"],
+    "external_dependencies": {"python": ["pycountry",],},
+    "data": ["views/country_view.xml",],
     "installable": True,
 }
