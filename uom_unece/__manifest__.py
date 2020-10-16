22d58f56ea22742b1f8d595211a110a7bc0804ab
Alexis de Lattre <alexis.delattre@akretion.com>
2020-10-16 12:27:58 +0200

    [MIG] uom_unece from v13 to v14

--- uom_unece/__manifest__.py
+++ uom_unece/__manifest__.py
@@ -1,18 +1,18 @@
-# Copyright 2016 Akretion (http://www.akretion.com)
+# Copyright 2016-2020 Akretion France (http://www.akretion.com)
 # @author Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
     "name": "Product UoM UNECE",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "category": "Sales",
     "license": "AGPL-3",
     "development_status": "Production/Stable",
     "summary": "UNECE nomenclature for the units of measure",
     "author": "Akretion,Odoo Community Association (OCA)",
-    "maintainers": ["astirpe"],
+    "maintainers": ["astirpe", "alexis-via"],
     "website": "https://github.com/OCA/community-data-files/",
     "depends": ["uom"],
-    "data": ["data/unece.xml", "views/product_uom.xml"],
+    "data": ["data/unece.xml", "views/uom_uom.xml"],
     "installable": True,
 }
