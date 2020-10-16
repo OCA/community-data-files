797620eddccd9851b9cb8cccd0d4c5b4e815c1fb
Alexis de Lattre <alexis.delattre@akretion.com>
2020-10-17 00:39:31 +0200

    [MIG] account_payment_unece from v13 to v14
    account_payment_unece: restore initial XML formatting

--- account_payment_unece/models/unece_code_list.py
+++ account_payment_unece/models/unece_code_list.py
@@ -1,4 +1,4 @@
-# Copyright 2016 Akretion (http://www.akretion.com)
+# Copyright 2016-2020 Akretion France (http://www.akretion.com)
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
@@ -9,5 +9,6 @@ class UneceCodeList(models.Model):
     _inherit = "unece.code.list"
 
     type = fields.Selection(
-        selection_add=[("payment_means", "Payment Means (UNCL 4461)")]
+        selection_add=[("payment_means", "Payment Means (UNCL 4461)")],
+        ondelete={"payment_means": "cascade"},
     )
