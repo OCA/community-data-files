14b13036579ec7a46d5a04ff7aaaf9aa10807bb6
Thierry Ducrest <thierry.ducrest@camptocamp.com>
2021-08-20 05:25:54 +0200

    [13.0][IMP] dangerous product add full name
    For the product.dangerous.class Having the name and the code
    in the UI when looking at a product is helpfull to the user.

--- l10n_eu_product_adr/models/product_dangerous.py
+++ l10n_eu_product_adr/models/product_dangerous.py
@@ -1,13 +1,15 @@
 # Copyright 2019 Iryna Vyshnevska (Camptocamp)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class ProductDangerousClass(models.Model):
     _name = "product.dangerous.class"
     _description = "Product Dangerous Class"
+    _rec_name = "name_full"
 
     name = fields.Char(required=True, translate=True)
+    name_full = fields.Char(compute="_compute_name_full")
     code = fields.Char(required=True)
     image = fields.Binary(string="Icon", required=True)
     class_type_id = fields.Many2one(
@@ -15,6 +17,11 @@ class ProductDangerousClass(models.Model):
         ondelete="restrict",
         string="Dangerous Type",
     )
+
+    @api.depends("name", "code")
+    def _compute_name_full(self):
+        for record in self:
+            record.name_full = " ".join([record.code, record.name])
 
     _sql_constraints = [("code_unique", "unique(code)", "This code already exist")]
 
