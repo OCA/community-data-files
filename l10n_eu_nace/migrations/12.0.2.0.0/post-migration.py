46e28efc78d2340c1514e9bd395f348df0657299
Florent THOMAS <florent.thomas@mind-and-go.com>
2021-08-21 11:22:57 +0200

    FIX Pylint

--- l10n_eu_nace/migrations/12.0.2.0.0/post-migration.py
+++ l10n_eu_nace/migrations/12.0.2.0.0/post-migration.py
@@ -8,22 +8,22 @@ def migrate(cr, version):
         """
         WITH imd_new AS(
             SELECT 
-                imd_new.name AS xml_id, 
+                imd_new.name AS xml_id,
                 imd_new.res_id AS new_res_id
             FROM ir_model_data AS imd_new
-            WHERE imd_new.name NOT LIKE 'old_%' 
+            WHERE imd_new.name NOT LIKE 'old_%'
             AND imd_new.module = 'l10n_eu_nace'
         ),
         imd_old AS(
-            SELECT 
-                substring(imd_old.name, 5) AS xml_id, 
+            SELECT
+                substring(imd_old.name, 5) AS xml_id,
                 imd_old.res_id AS old_res_id
             FROM ir_model_data AS imd_old
-            WHERE imd_old.name LIKE 'old_%' 
+            WHERE imd_old.name LIKE 'old_%'
             AND imd_old.module = 'l10n_eu_nace'
         ),
         match_old_new AS(
-            SELECT 
+            SELECT
                 imd_new.xml_id,
                 imd_new.new_res_id,
                 imd_old.old_res_id
@@ -31,15 +31,15 @@ def migrate(cr, version):
             INNER JOIN imd_old ON imd_new.xml_id = imd_old.xml_id
         ),
         partner_with_old_nace AS(
-            SELECT 
+            SELECT
                 rp.id AS part_id,
                 rp.name,
                 rpc.category_id,
                 match_old_new.new_res_id
             FROM res_partner AS rp
-            INNER JOIN res_partner_res_partner_category_rel AS rpc ON rpc.partner_id = rp.id
+            INNER JOIN res_partner_res_partner_category_rel AS rpc 
+                                     ON rpc.partner_id = rp.id
             INNER JOIN match_old_new ON match_old_new.old_res_id = rpc.category_id
-            
         )
         UPDATE res_partner rp
         SET nace_id = partner_with_old_nace.new_res_id
