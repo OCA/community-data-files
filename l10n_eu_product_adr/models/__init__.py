1f6be7f37acecd6d7ef6498462e40cc59af5781e
Stefan Rijnhart <stefan@opener.am>
2021-11-17 18:44:57 +0100

    [RFR] l10n_eu_product_adr: allow different adr goods per variant

--- l10n_eu_product_adr/models/__init__.py
+++ l10n_eu_product_adr/models/__init__.py
@@ -3,6 +3,7 @@ from . import adr_goods
 from . import adr_label
 from . import adr_packing_instruction
 from . import common
+from . import product_product
 from . import product_template
 from . import stock_move
 from . import stock_picking
