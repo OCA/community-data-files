111b77b18ead7ab2ec9d4d4133b1ae776d1c1260
Alexis de Lattre <alexis.delattre@akretion.com>
2021-12-06 19:36:43 +0100

    [MIG] account_payment_unece from v14 to v15

--- account_payment_unece/__manifest__.py
+++ account_payment_unece/__manifest__.py
@@ -1,10 +1,10 @@
-# Copyright 2016-2020 Akretion France (http://www.akretion.com)
+# Copyright 2016-2021 Akretion France (http://www.akretion.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 # @author Alexis de Lattre <alexis.delattre@akretion.com>
 
 {
     "name": "Account Payment UNECE",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "category": "Accounting & Finance",
     "license": "AGPL-3",
     "summary": "UNECE nomenclature for the payment methods",
