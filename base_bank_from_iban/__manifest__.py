91b229fec2c5254e86dc87b9dfe2443998a62d35
Pedro Evaristo Gonzalez Sanchez <pedro.gonzalez@pesol.es>
2022-10-19 10:40:21 -0400

    [MIG] base_bank_from_iban: Migration to 16.0

--- base_bank_from_iban/__manifest__.py
+++ base_bank_from_iban/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Bank from IBAN",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "author": "Tecnativa, " "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/community-data-files",
     "category": "Localization",
