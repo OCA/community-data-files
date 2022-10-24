efe2b2d2b9f205055cbb17b826187aa212bfd631
sergio-teruel <sergio.teruel@tecnativa.com>
2022-10-24 20:17:36 +0200

    [IMP] product_fao_fishing: Remove FAO technique as a model. Use attribute values like as fishing areas

--- product_fao_fishing/tests/test_product_fao_fishing.py
+++ product_fao_fishing/tests/test_product_fao_fishing.py
@@ -1,6 +1,5 @@
 # Copyright 2018 Tecnativa - Sergio Teruel
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-from odoo.exceptions import UserError
 from odoo.tests import SavepointCase
 
 
@@ -9,11 +8,20 @@ class TestProductFaoFishing(SavepointCase):
     def setUpClass(cls):
         super().setUpClass()
         cls.product_attribute = cls.env.ref("product_fao_fishing.fao_fishing_area")
+        cls.fao_technique_attribute = cls.env.ref(
+            "product_fao_fishing.fao_fishing_technique"
+        )
         cls.product_attribute_value_1 = cls.env.ref(
             "product_fao_fishing.fao_fishing_area_1"
         )
         cls.product_attribute_value_2 = cls.env.ref(
             "product_fao_fishing.fao_fishing_area_2"
+        )
+        cls.technique_attribute_value_1 = cls.env.ref(
+            "product_fao_fishing.fishing_technique_spearfishing"
+        )
+        cls.technique_attribute_value_2 = cls.env.ref(
+            "product_fao_fishing.fishing_technique_harrows"
         )
         cls.product = cls.env["product.template"].create(
             {
@@ -29,29 +37,28 @@ class TestProductFaoFishing(SavepointCase):
                                 (4, cls.product_attribute_value_2.id),
                             ],
                         },
-                    )
+                    ),
+                    (
+                        0,
+                        0,
+                        {
+                            "attribute_id": cls.fao_technique_attribute.id,
+                            "value_ids": [
+                                (4, cls.technique_attribute_value_1.id),
+                                (4, cls.technique_attribute_value_2.id),
+                            ],
+                        },
+                    ),
                 ],
             }
         )
         cls.fishing_tech_arrow = cls.env.ref(
             "product_fao_fishing.fishing_technique_harrows"
         )
-        cls.my_tech = cls.env["product.fao.fishing.technique"].create(
-            {"name": "My Tech", "parent_id": cls.fishing_tech_arrow.id}
-        )
-
-    def test_fishing_technique_name(self):
-        self.assertEqual(
-            self.my_tech.complete_name,
-            "{} / {}".format(self.fishing_tech_arrow.name, self.my_tech.name),
-        )
-
-    def test_recursion(self):
-        with self.assertRaises(UserError):
-            self.my_tech.parent_id = self.my_tech
 
     def test_fishing_areas_from_attribute(self):
         """
         Test helper method to access directly to FAO fishing area product
         """
         self.assertEqual(len(self.product.fao_fishing_area_ids), 2)
+        self.assertEqual(len(self.product.fao_fishing_technique_ids), 2)
