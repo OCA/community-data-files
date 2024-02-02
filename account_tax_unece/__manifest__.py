e5d1b2f61aa38a58b837d149bd1a2e6cb15ed3a8
chien <chien@trobz.com>
2024-02-02 14:51:10 +0700

    [MIG] account_tax_unece: Migration to 17.0

--- account_tax_unece/__manifest__.py
+++ account_tax_unece/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Account Tax UNECE",
-    "version": "16.0.1.3.0",
+    "version": "17.0.1.0.0",
     "category": "Accounting & Finance",
     "license": "LGPL-3",
     "development_status": "Production/Stable",
@@ -15,7 +15,6 @@
     "depends": ["account", "base_unece"],
     "data": [
         "views/account_tax.xml",
-        "views/account_tax_template.xml",
         "data/unece_tax_type.xml",
         "data/unece_tax_categ.xml",
     ],
--- account_tax_unece/models/__init__.py
+++ account_tax_unece/models/__init__.py
@@ -1,4 +1,3 @@
 from . import unece_code_list
 from . import account_tax
-from . import account_tax_template
 from . import res_company
