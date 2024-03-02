82371889c1b040041107d73dbfc6c5c7d7c7b13c
Alexis de Lattre <alexis.delattre@akretion.com>
2024-03-02 18:28:09 +0100

    [MIG] account_payment_unece from v16 to v17

--- account_payment_unece/__manifest__.py
+++ account_payment_unece/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Account Payment UNECE",
-    "version": "16.0.1.0.0",
+    "version": "17.0.1.0.0",
     "category": "Accounting & Finance",
     "license": "AGPL-3",
     "summary": "UNECE nomenclature for the payment methods",
