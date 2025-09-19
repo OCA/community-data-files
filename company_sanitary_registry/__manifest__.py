6e37a9172aee2ab7d601928d7d58dcbcc3fcd173
CarlosRoca13 <carlos.roca@tecnativa.com>
2025-09-19 12:10:06 +0200

    [IMP] company_sanitary_registry: Add model to manage multiple sanitary registries and add it to the product categories

--- company_sanitary_registry/__manifest__.py
+++ company_sanitary_registry/__manifest__.py
@@ -2,11 +2,14 @@
 # License AGPL-3 - See https://www.gnu.org/licenses/agpl-3.0.html
 {
     "name": "Sanitary Registry",
-    "version": "16.0.1.0.0",
-    "depends": ["base"],
+    "version": "16.0.1.1.0",
+    "depends": ["product"],
     "data": [
+        "security/ir.model.access.csv",
         "templates/sanitary_registry_template.xml",
+        "views/product_category_view.xml",
         "views/res_company_view.xml",
+        "views/sanitary_registry_view.xml",
     ],
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/community-data-files",
