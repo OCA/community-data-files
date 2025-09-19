566b64cf0a9d65a62849c658a13c653db5a4d514
CarlosRoca13 <carlos.roca@tecnativa.com>
2025-09-19 12:10:06 +0200

    [IMP] company_sanitary_registry: Add model to manage multiple sanitary registries and add it to the product categories

--- company_sanitary_registry/models/res_company.py
+++ company_sanitary_registry/models/res_company.py
@@ -6,4 +6,7 @@ from odoo import fields, models
 class ResCompany(models.Model):
     _inherit = "res.company"
 
-    sanitary_registry = fields.Char()
+    sanitary_registry_id = fields.Many2one("sanitary.registry")
+    # Field to avoid crashes modules that uses yet this field. To be removed
+    # On future versions.
+    sanitary_registry = fields.Char(related="sanitary_registry_id.name")
